f1a40b73a8ddcfa46033f118226212795c431575
Be-win <bewinpg@gmail.com>
2025-01-18 19:41:03 +0530

    binary_decimal

--- binaryToDecimal.py
+++ binaryToDecimal.py
@@ -4,6 +4,9 @@ decimal = 0
 power = 0
 
 for char in binary[::-1]:
+    if int(char) > 1:
+        print("Invalid binary input!")
+        exit()
     decimal += int(char) * 2 ** power
     power += 1
 
